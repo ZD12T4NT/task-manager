eab91fed1be8d4046b9fed87e6343a78c22e8fe7
Zac Distant <zacdistant@gmail.com>
2022-05-01 13:58:00 +0100

    Appropriate folders created

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Left } from "./Left";
+
+export const Home = () => {
+  return (
+      <>
+     <Left />
+
+   
+      </>
+     
+  )
+};
---
+++ src/components/Left.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Left = () => {
+  return(
+      <LeftNav>
+          
+      </LeftNav>
+  )
+};
